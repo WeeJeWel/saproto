bd6ec2ea1a44509e6ec3b799b7f771669730d65b
Laura <laubbblub@gmail.com>
2018-04-10 19:26:29 +0200

    Edited the boards list

--- resources/views/boards/list.blade.php
+++ resources/views/boards/list.blade.php
@@ -1,7 +1,7 @@
 @extends('website.layouts.default-nobg')
 
 @section('page-title')
-    Boards
+    Previous Boards
 @endsection
 
 @section('content')
@@ -11,7 +11,9 @@
 
         <a href="{{ route('board::show', ['id' => $board->id]) }}" class="committee-link">
             <div class="committee"
-                 style="{{ ($board->image ? "background-image: url(".$board->image->generateImagePath(450, 300).");" : '') }}">
+                 {{--style="{{ ($board->image ? "background-image: url(".$board->image->generateImagePath(450, 300).");" : '') }}"--}}
+                style = "background-image:url('http://www.takepart.com/sites/default/files/styles/large/public/wombat-MAIN.jpg');"
+            >
                 <div class="committee-name">
                     {{ $board->edition }}
                 </div>
@@ -23,3 +25,44 @@
 
 @endsection
 
+@section('stylesheet')
+
+    @parent
+
+    <style type="text/css">
+
+        .committee {
+            position: relative;
+            width: 100%;
+            height: 200px;
+
+            background-color: #666;
+            background: linear-gradient(to bottom right, #333, #666);
+            background-size: cover;
+            background-position: center center;
+
+            margin-bottom: 30px;
+        }
+
+        .committee:hover {
+            transform: scale(1.05,1.05);
+        }
+        .committee-name {
+            position: absolute;
+            bottom: 0;
+
+            background-color: rgba(0, 0, 0, 0.7);
+            color: #fff;
+            padding: 10px 30px;
+        }
+
+        .committee-link:hover {
+            text-decoration: none;
+
+        }
+
+
+
+    </style>
+
+@endsection
--- resources/views/committee/show.blade.php
+++ resources/views/committee/show.blade.php
@@ -21,16 +21,10 @@
 
                     {!! Markdown::convertToHtml($committee->description) !!}
 
-                    <hr>
-
-                    If you want, you can e-mail them at
-                    <a href="mailto:{{ $committee->slug . "@" . config('proto.emaildomain') }}">
-                        {{ $committee->slug . "@" . config('proto.emaildomain') }}
-                    </a>.
 
                 </div>
 
-                @if(Auth::check() && ($committee->allow_anonymous_email || Auth::user()->can('board')))
+                @if(Auth::check() &&  Auth::user()->can('board'))
 
                     <div class="panel-footer clearfix">
 
@@ -43,211 +37,15 @@
 
                         @endif
 
-                        @if($committee->allow_anonymous_email)
 
-                            <a href="{{ route("committee::anonymousmail", ["id" => $committee->id]) }}"
-                               class="btn btn-info">
-                                Send this committee an anonymous e-mail
-                            </a>
-
-                        @endif
 
                     </div>
 
                 @endif
 
             </div>
-
-            @if(Auth::check() && Auth::user()->member)
-
-                @if(count($committee->upcomingEvents()) > 0)
-
-                    <div class="panel panel-default calendar">
-
-                        <div class="panel-heading">
-                            Upcoming Events
-                        </div>
-
-                        <div class="panel-body">
-
-                            <div class="row">
-
-                                @foreach($committee->upcomingEvents() as $key => $event)
-
-                                    <div class="col-md-6">
-
-                                        <a class="activity"
-                                           href="{{ route('event::show', ['id' => $event->id]) }}">
-                                            <div class="activity {{ ($key % 2 == 1 ? 'odd' : '') }}" {!! ($event->secret ? 'style="opacity: 0.3;"' : '') !!}>
-                                                <p><strong>{{ $event->title }}</strong></p>
-                                                <p><i class="fa fa-map-marker"
-                                                      aria-hidden="true"></i> {{ $event->location }}
-                                                </p>
-                                                <p>
-                                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
-                                                    {{ date('l j F', $event->start) }}, {{ date('H:i', $event->start) }}
-                                                    -
-                                                    @if (($event->end - $event->start) < 3600 * 24)
-                                                        {{ date('H:i', $event->end) }}
-                                                    @else
-                                                        {{ date('j M, H:i', $event->end) }}
-                                                    @endif
-                                                </p>
-                                            </div>
-                                        </a>
-
-                                    </div>
-
-                                @endforeach
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                @endif
-
-                @if(count($committee->pastEvents()) > 0)
-
-                    @php
-                        $year = date('Y', $committee->pastEvents()[0]->start)
-                    @endphp
-
-                    <div class="panel panel-default calendar">
-
-                        <div class="panel-heading">
-                            Past Events ({{ $year }})
-                        </div>
-
-                        <div class="panel-body">
-
-                            <div class="row">
-
-                                @foreach($committee->pastEvents() as $key => $event)
-
-                                    @php
-                                        $newyear = date('Y', $event->start);
-                                    @endphp
-
-                                    @if($newyear != $year)
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div class="panel panel-default calendar">
-
-                        <div class="panel-heading">
-                            Past Events ({{ $newyear }})
-                        </div>
-
-                        <div class="panel-body">
-
-                            <div class="row">
-
-                                @endif
-
-                                @php
-                                    $year = $newyear;
-                                @endphp
-
-                                <div class="col-md-6">
-
-                                    <a class="activity"
-                                       href="{{ route('event::show', ['id' => $event->id]) }}">
-                                        <div class="activity" {!! ($event->secret ? 'style="opacity: 0.3;"' : '') !!}>
-                                            <p><strong>{{ $event->title }}</strong></p>
-                                            <p>
-                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                                {{ $event->location }}
-                                            </p>
-                                            <p>
-                                                <sup>
-                                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
-                                                    {{ date('l j F', $event->start) }}, {{ date('H:i', $event->start) }}
-                                                    -
-                                                    @if (($event->end - $event->start) < 3600 * 24)
-                                                        {{ date('H:i', $event->end) }}
-                                                    @else
-                                                        {{ date('j M, H:i', $event->end) }}
-                                                    @endif
-                                                </sup>
-                                            </p>
-                                        </div>
-                                    </a>
-
-                                </div>
-
-                                @endforeach
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                @endif
-
-                @if(count($committee->helpedEvents()) > 0)
-
-                    <div class="panel panel-default calendar">
-
-                        <div class="panel-heading">
-                            Helped Events
-                        </div>
-
-                        <div class="panel-body">
-
-                            <div class="row">
-
-                                @foreach($committee->helpedEvents() as $key => $event)
-
-                                    @if($key % 2 == 0 && $key > 1)
-                                        <? echo '</div><div class="row">'?>
-                                    @endif
-
-                                    <div class="col-md-6">
-
-                                        <a class="activity"
-                                           href="{{ route('event::show', ['id' => $event['id']]) }}">
-                                            <div class="activity" {!! ($event['secret'] ? 'style="opacity: 0.3;"' : '') !!}>
-                                                <p><strong>{{ $event['title'] }}</strong></p>
-                                                <p><i class="fa fa-map-marker"
-                                                      aria-hidden="true"></i> {{ $event['location'] }}
-                                                </p>
-                                                <p>
-                                                    <sup>
-                                                        <i class="fa fa-clock-o" aria-hidden="true"></i>
-                                                        {{ date('l j F Y', $event['start']) }}
-                                                        , {{ date('H:i', $event['start']) }}
-                                                        -
-                                                        @if (($event['end'] - $event['start']) < 3600 * 24)
-                                                            {{ date('H:i', $event['end']) }}
-                                                        @else
-                                                            {{ date('j M, H:i', $event['end']) }}
-                                                        @endif
-                                                    </sup>
-                                                </p>
-                                            </div>
-                                        </a>
-
-                                    </div>
-
-                                @endforeach
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                @endif
-
-            @endif
-
         </div>
+
 
         @if(Auth::check() && Auth::user()->member)
 
@@ -267,6 +65,7 @@
             </div>
 
         @endif
+
 
     </div>
 
